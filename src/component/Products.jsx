1ea96d1610af9e3df835c5b20dfcd8e7ae7892d6
Jancy Febin <jancydevassy555@gmail.com>
2022-11-11 17:26:07 +0530

    second

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -16,11 +16,16 @@ const Products = () => {
               {/* product image */}
               <div className="relative">
                 <img src={img} alt="" className="w-full" />
-                <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
-                <div className="flex items-center gap-2">
-                  <span className="badge rotate-90">New</span>
+                <div className="absolute inset-0  flex items-center justify-end  ">
+                {/* <div className="flex items-center "> */}
+                  {/* <span className="badge rotate-90">New</span> */}
+                  <div className="mb-14">
+                  <span class="w-8 p-0.5  bg-black "></span>
+
+                  </div>
+
                   <span className="badge rotate-90">Trending</span>
-                </div>
+                {/* </div> */}
                   {/* <a
                     href=""
                     className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
@@ -38,6 +43,7 @@ const Products = () => {
 
               {/* After image */}
               <div className="p-5 flex-col gap-3">
+
                 {/* badge */}
                 {/* <div className="flex items-center gap-2">
                   <span className="badge rotate-90">New</span>
